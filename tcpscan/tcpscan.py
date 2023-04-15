1d094c6cf694d9fe9fe2d81cbfb39d6a146b8e52
smith-user <andrey2003030@gmail.com>
2023-04-16 02:17:41 +0500

    tcpscan

---
+++ tcpscan/tcpscan.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+import socket
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='TCP сканнер')
+parser.add_argument('--addr', type=str, help='ip или доменное имя', default='127.0.0.1')
+parser.add_argument('--min', type=int, help='нижнее значение портов', default=1)
+parser.add_argument('--max', type=int, help='верхнее значение портов', default=65535)
+
+args = parser.parse_args().__dict__
+
+addr = socket.gethostbyname(args['addr'])
+ports_range = (args['min'], args['max'])
+
+print(f'Доступные порты {addr}.')
+for port in range(ports_range[0], ports_range[1]):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(5)
+        if s.connect_ex((addr, port)) == 0:
+            print(f'{port}'.rjust(6))
