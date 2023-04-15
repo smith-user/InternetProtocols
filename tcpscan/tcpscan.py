57fb88d56e97eb5d43046948d09b7d3bb872a0cb
smith-user <andrey2003030@gmail.com>
2023-04-16 02:23:04 +0500

    tcpscan refactoring

--- tcpscan/tcpscan.py
+++ tcpscan/tcpscan.py
@@ -4,14 +4,12 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description='TCP сканнер')
 parser.add_argument('--addr', type=str, help='ip или доменное имя', default='127.0.0.1')
-parser.add_argument('--min', type=int, help='нижнее значение портов', default=1)
-parser.add_argument('--max', type=int, help='верхнее значение портов', default=65535)
+parser.add_argument('--min', type=int, help='нижняя граница диапазона портов', default=1)
+parser.add_argument('--max', type=int, help='верхняя граница диапазона портов', default=65535)
 
 args = parser.parse_args().__dict__
-
 addr = socket.gethostbyname(args['addr'])
 ports_range = (args['min'], args['max'])
-
 print(f'Доступные порты {addr}.')
 for port in range(ports_range[0], ports_range[1]):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
